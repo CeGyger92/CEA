411f8a429726c471947c70d99feb216322328a31
CeGyger92 <42616034+CeGyger92@users.noreply.github.com>
2020-11-02 09:56:30 -0600

    NgFor on Production Results

--- src/app/productions/productions.component.ts
+++ src/app/productions/productions.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ProductionsComponent implements OnInit {
-
+ results: string[];
   constructor() {
-
+    this.results = ["Result1", "Result2", "Result3", "Results4"];
   }
 
   ngOnInit(): void {
